28a2d4c08104fc5be773744b037b59930ac19be2
Volodymyr <zmiyov88@gmail.com>
2024-05-21 17:33:51 +0300

    Initial Commit

---
+++ SharediOsMacOSLogin/SharediOsMacOSLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharediOsMacOSLoginApp.swift
+//  SharediOsMacOSLogin
+//
+//  Created by Vladimir Pisarenko on 21.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SharediOsMacOSLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
